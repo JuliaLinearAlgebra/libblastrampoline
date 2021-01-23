25783d9037ef5a90bf52da5e576229635c47a3f1
Viral B. Shah <viral@mayin.org>
2021-01-22 20:11:36 -0500

    Add all the symbols

---
+++ gensymbol/gensymbols.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+perl ./gensymbol osx x86_64 _ 1 0  0 0 0 0 "" "" 1 0 1 1 1 1 > tempsymbols.def
+
+echo "#define JL_EXPORTED_FUNCS(XX) \\"
+for s in `cut -b2-100 tempsymbols.def`; do
+    echo XX\($s\) \\;
+done
+
+rm -f *.def
