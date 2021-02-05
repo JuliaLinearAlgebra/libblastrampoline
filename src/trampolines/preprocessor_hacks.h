85f3509b94240967a4c3e4ff8fd4c41afe292877
Elliot Saba <staticfloat@gmail.com>
2021-02-05 20:37:28 +0000

    Factor out aarch64 PAGE definitions

--- src/trampolines/preprocessor_hacks.h
+++ src/trampolines/preprocessor_hacks.h
@@ -39,15 +39,28 @@
 #define SEH_END()
 #endif
 
-
 // If we're compiling with control-flow branch protection, mark the trampoline entry
 // points with `endbr{32,64}`, as appropriate on this arch
 #if defined(__CET__) && __CET__ & 1 != 0
-#ifdef __x86_64__
+#if defined(__x86_64__)
 #define CET_START()     endbr64
 #else
 #define CET_START()     endbr32
 #endif
 #else
 #define CET_START()
+#endif
+
+// aarch64 on mac requires some special assembler syntax for both calculating memory
+// offsets and even just the assembler statement separator token
+#if defined(__aarch64__)
+#if defined(__APPLE__)
+#define PAGE(x)     x##@PAGE
+#define PAGEOFF(x)  x##@PAGEOFF
+#define SEP         %%
+#else
+#define PAGE(x)     x
+#define PAGEOFF(x)  #:lo12:##x
+#define SEP         ;
+#endif
 #endif
