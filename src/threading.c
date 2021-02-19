e8297a4a3da9d35c5cc45954aeb6585f19b789b4
Elliot Saba <staticfloat@gmail.com>
2021-02-19 06:56:46 +0000

    Allow for user-registered thread set/get methods

--- src/threading.c
+++ src/threading.c
@@ -1,11 +1,50 @@
 #include "libblastrampoline_internal.h"
 
-static const char * getter_names[] = {
+#define MAX_THREADING_NAMES     32
+
+/*
+ * We provide a flexible thread getter/setter interface here; by calling `lbt_set_num_threads()`
+ * libblastrampoline will propagate the call through to its loaded libraries as long as the
+ * library exposes a getter/setter method named in one of these arrays, and its signature
+ * matches the common theme here.
+ */
+static char * getter_names[MAX_THREADING_NAMES] = {
     "openblas_get_num_threads",
-    "openblas_get_num_threads_64",
     "mkl_get_max_threads",
+    "bli_thread_set_num_threads",
     NULL
 };
+
+static char * setter_names[MAX_THREADING_NAMES] = {
+    "openblas_set_num_threads",
+    "mkl_set_num_threads",
+    "bli_thread_get_num_threads",
+    NULL
+};
+
+/*
+ * If you have a truly custom BLAS, you can pass in the explicit getter/setter method names here.
+ * Note that these names will have the library suffix appended to them!
+ */
+JL_DLLEXPORT void lbt_register_thread_interface(const char * getter, const char * setter) {
+    int idx = 0;
+    while (getter_names[idx] != NULL) {
+        // We refuse to register ridiculous amounts of these
+        if (idx >= MAX_THREADING_NAMES - 1) {
+            return;
+        }
+        // We don't allow duplicates
+        if (strcmp(getter_names[idx], getter) == 0 && strcmp(setter_names[idx], setter) == 0) {
+            return;
+        }
+        idx++;
+    }
+
+    getter_names[idx] = (char *) malloc(strlen(getter));
+    strcpy(getter_names[idx], getter);
+    setter_names[idx] = (char *) malloc(strlen(setter));
+    strcpy(setter_names[idx], setter);
+}
 
 /*
  * Returns the number of threads configured in all loaded libraries.
@@ -31,12 +70,6 @@ JL_DLLEXPORT int32_t lbt_get_num_threads() {
 }
 
 
-static const char * setter_names[] = {
-    "openblas_set_num_threads",
-    "openblas_set_num_threads_64",
-    "mkl_set_num_threads",
-    NULL
-};
 
 /*
  * Sets the given number of threads for all loaded libraries.
