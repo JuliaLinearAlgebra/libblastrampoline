c0403b8acee6eaaed988b85cd5684db7d3f823f9
Elliot Saba <staticfloat@gmail.com>
2022-01-29 01:46:59 +0000

    Remove vestigial testing code

--- test/runtests.jl
+++ test/runtests.jl
@@ -50,7 +50,7 @@ function run_test((test_name, test_expected_outputs, test_success), libblas_name
         p, output = capture_output(addenv(cmd, env))
 
         expected_return_value = success(p) ^ test_success
-        if !expected_return_value || rand() < 0.5
+        if !expected_return_value
             @error("Test failed", env)
             println(output)
         end
